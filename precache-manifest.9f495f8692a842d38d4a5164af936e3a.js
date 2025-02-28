bbb72bb90ef99977b8329fe28c826920f9f8ceeb
ramyabharathi <ramyabharathi.242002@gmail.com>
2025-02-28 21:59:01 +0530

    Updates

---
+++ precache-manifest.9f495f8692a842d38d4a5164af936e3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde42678e77c38aa37feb44b2f39e7ae",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "33514fe9cddcc7dc5bec",
+    "url": "/my-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "428b21f38b4891b377c1",
+    "url": "/my-portfolio/static/js/2.245ae556.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/my-portfolio/static/js/2.245ae556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "638bd94f11e257bee422",
+    "url": "/my-portfolio/static/js/3.e6adb5ee.chunk.js"
+  },
+  {
+    "revision": "33514fe9cddcc7dc5bec",
+    "url": "/my-portfolio/static/js/main.f972c774.chunk.js"
+  },
+  {
+    "revision": "9e76c15f2236052ca02a",
+    "url": "/my-portfolio/static/js/runtime-main.4e57d8da.js"
+  }
+]);
